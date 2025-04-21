55013805698c3af94b34fb0573665db58fb7424e
shenmar bonifacio <shenmarbonifacio@gmail.com>
2025-04-22 02:40:40 +0800

    updated profile

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
